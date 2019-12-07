3fc31ee2c9aa6691746e3a6373de463210bbe620
Thomas Lekanger <datagutt@lekanger.no>
2019-12-07 22:39:47 +0100

    Fix avatar path

--- src/routes/avatars/get.js
+++ src/routes/avatars/get.js
@@ -6,8 +6,8 @@ const fs = require('fs').promises;
 import userModel from '../../models/user';
 
 const sendFile = async (file, res) => {
-    console.log(`${global.nconf.get('base_dir')}/public/avatars/${file}`);
-    let asset = await fs.readFile(`${global.nconf.get('base_dir')}/public/avatars/${file}`, 'binary');
+    console.log(`${global.nconf.get('base_dir')}/../public/avatars/${file}`);
+    let asset = await fs.readFile(`${global.nconf.get('base_dir')}/../public/avatars/${file}`, 'binary');
     res.setHeader('Content-Type', `image/png; charset=utf-8`);
     return send(res, asset ? 200 : 404, asset ? new Buffer(data) : '')
 };
