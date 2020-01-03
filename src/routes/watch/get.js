5a4f26d0c7c5e118fd219749cb404684042b79db
datagutt <datagutt@lekanger.no>
2020-01-03 23:13:35 +0100

    Watch route: Add private flag

--- src/routes/watch/get.js
+++ src/routes/watch/get.js
@@ -31,6 +31,7 @@ module.exports = compose(
 					title: result.title,
 					live: parseInt(result.live, 10),
 					views: parseInt(result.views, 10),
+					private: result.private,
 					category_id: result.category_id,
 					category_name: result.category_name,
 					urls: {
