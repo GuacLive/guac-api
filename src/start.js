318076e25f81013b1d3f5305098c2684335e3803
Thomas Lekanger <datagutt@lekanger.no>
2020-02-28 00:21:01 +0100

    Update start.js

--- src/start.js
+++ src/start.js
@@ -1,6 +1,7 @@
 require('@babel/register');
+const http = require('http');
 const micro = require('micro');
 const index = require('./index');
 
-const server = micro(index);
+const server = new http.Server(micro(index));
 server.listen(9000);
