936959fa65cc4479e6c8d2eae2abba912ec65bfa
Thomas Lekanger <datagutt@lekanger.no>
2020-03-18 19:00:22 +0100

    Messages: Maybe it works now

--- src/routes/messages/get.js
+++ src/routes/messages/get.js
@@ -11,28 +11,19 @@ module.exports = compose(
 	async (req, res) => {
 		console.log('messages', req.params);
 		if(req.params && req.params.name && USERNAME_REGEX.test(req.params.name)){
-			fetch(`${global.nconf.get('server:chat_url')}/messages/${req.params.name}`,
+			let api = fetch(`${global.nconf.get('server:chat_url')}/messages/${req.params.name}`,
 				{
 					method: 'get',
 					headers: {
 						'Content-Type': 'application/json'
 					}
 				}
-			)
-			.then(res => res.json())
-			.then(
-				data => {
-					return send(res, 200, {
-						statusCode: 200,
-						data
-					});
-				},
-				err => {
-					return send(res, 500, {
-						statusCode: 500,
-					});
-				}
 			);
+			const data = await api.json();
+			return send(res, 200, {
+					statusCode: 200,
+					data
+			});
 		}else{
 			return send(res, 200, {
 				statusCode: 200,
