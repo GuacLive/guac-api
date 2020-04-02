830d21523bb4275915c4995f89cf141e268c41d3
Thomas Lekanger <datagutt@lekanger.no>
2020-04-02 19:28:10 +0200

    Add version number to index

--- src/routes/index/get.js
+++ src/routes/index/get.js
@@ -1,4 +1,10 @@
 const { send } = require('micro');
-const { compose, respondToLivenessProbe } = require('micro-hoofs');
+const { compose } = require('micro-hoofs');
 
-module.exports = compose(respondToLivenessProbe)();
+module.exports = compose(
+)(async (req, res) => {
+    return send(res, 200, {
+        healthy: true,
+        version: process.env.npm_package_version
+    });
+});
