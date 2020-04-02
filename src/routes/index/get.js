84b463dc245e1c21d7674575c4fa6f154285dccb
Thomas Lekanger <datagutt@lekanger.no>
2020-04-02 19:32:36 +0200

    Update get.js

--- src/routes/index/get.js
+++ src/routes/index/get.js
@@ -1,10 +1,11 @@
 const { send } = require('micro');
 const { compose } = require('micro-hoofs');
-
+const version = require('../../../package.json').version;
 module.exports = compose(
 )(async (req, res) => {
     return send(res, 200, {
         healthy: true,
-        version: process.env.npm_package_version
+        version: version ? version : process.env.npm_package_version,
+        statusCode: 200
     });
 });
