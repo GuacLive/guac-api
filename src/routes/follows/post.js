004ed58073168551aea4ea6e55f5b5a82f219beb
datagutt <datagutt@lekanger.no>
2020-06-09 16:11:24 +0200

    Update post.js

--- src/routes/follows/post.js
+++ src/routes/follows/post.js
@@ -26,7 +26,7 @@ module.exports = compose(
 			});
 		}
 
-		result = result.map((r) => {
+		result = result.map(async (r) => {
 			if(r){
 				r.avatar = r.avatar || `//api.${global.nconf.get('server:domain')}/avatars/unknown.png`;
 				if(r.live){
