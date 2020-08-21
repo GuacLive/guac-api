fc4a601f4735d4bbcee7668135b493a48696e3a9
Thomas Lekanger <datagutt@lekanger.no>
2020-08-21 18:53:17 +0200

    Follows: Experiments

--- src/routes/follows/post.js
+++ src/routes/follows/post.js
@@ -6,42 +6,46 @@ import channelModel from '../../models/channel';
 import verifyJWTKey from '../../services/verifyJWTKey';
 
 import { getFromViewerAPI } from '../../utils';
+async function follows(req, res){
+	const channel = new channelModel;
+	const jsonData = await json(req);
+	let result;
+	if (typeof jsonData.from_id !== 'undefined') {
+		result = await channel.getFollowsFrom(jsonData.from_id);
+	} else if (typeof jsonData.from_id !== 'undefined') {
+		result = await channel.getFollowsTo(jsonData.to_id);
+	} else if (req.user && typeof req.user.id !== 'undefined') {
+		result = await channel.getFollowsFrom(req.user.id);
+	} else {
+		return send(res, 400, {
+			statusCode: 400,
+			statusMessage: 'No from_id or to_id given'
+		});
+	}
+
+	result = await Promise.all(result.map(async (r) => {
+		if (r) {
+			r.avatar = r.avatar || `//api.${global.nconf.get('server:domain')}/avatars/unknown.png`;
+			if (r.live) {
+				r.viewers = await getFromViewerAPI(r.username);
+			} else {
+				r.viewers = 0;
+			}
+		}
+		return r;
+	}));
+	send(res, 200, {
+		statusCode: 200,
+		data: result
+	});
+}
 module.exports = compose(
 )(
 	async (req, res) => {
 		if(req.headers.authorization){
-			await verifyJWTKey();
-		}
-		const channel = new channelModel;
-		const jsonData = await json(req);
-		let result;
-		if(typeof jsonData.from_id !== 'undefined'){
-			result = await channel.getFollowsFrom(jsonData.from_id);
-		}else if(typeof jsonData.from_id !== 'undefined'){
-			result = await channel.getFollowsTo(jsonData.to_id);
-		}else if(req.user && typeof req.user.id !== 'undefined'){
-			result = await channel.getFollowsFrom(req.user.id);
+			await verifyJWTKey(() => follows(req,res));
 		}else{
-			return send(res, 400, {
-				statusCode: 400,
-				statusMessage: 'No from_id or to_id given'
-			});
+			await follows(req,res);
 		}
-
-		result = await Promise.all(result.map(async (r) => {
-			if(r){
-				r.avatar = r.avatar || `//api.${global.nconf.get('server:domain')}/avatars/unknown.png`;
-				if(r.live){
-					r.viewers = await getFromViewerAPI(r.username);
-				}else{
-					r.viewers = 0;
-				}
-			}
-			return r;
-		}));
-		send(res, 200, {
-			statusCode: 200,
-			data: result
-		});
 	}
 );
