1e9c5cd56c5ca486b9199e6109fe18be779df46c
Thomas Lekanger <datagutt@lekanger.no>
2021-02-10 13:04:23 +0100

    Update saveAvatar.js

--- src/routes/avatar/saveAvatar.js
+++ src/routes/avatar/saveAvatar.js
@@ -1,43 +1,44 @@
 import { send } from 'micro';
 import { compose } from 'micro-hoofs';
 
-const fs = require('fs').promises;
+const { Readable } = require('stream');
 
 import userModel from '../../models/user';
 import verifyJWTKey from '../../services/verifyJWTKey';
 import uploadService from '../../services/upload';
 module.exports = compose(
-    verifyJWTKey,
-    uploadService
+	verifyJWTKey,
+	uploadService
 )(
 	async (req, res) => {
-        const um = new userModel;
-        console.log(req.s3, req.file);
-        if(!req.user.id){
+		const um = new userModel;
+		console.log(req.s3, req.file);
+		if(!req.user.id){
 			return send(res, 403, {
 				statusCode: 403,
 			});
-        }
-        if(!req.s3 || !req.file || !req.file.buffer){
+		}
+		if(!req.s3 || !req.file || !req.file.buffer){
 			return send(res, 400, {
 				statusCode: 400,
 			});
-        }
-        const { profilePicBlobStore } = req.s3;
-        console.log('profilePicBlobStore', profilePicBlobStore);
-        
-        fs.createReadStream(req.file.buffer).pipe(profilePicBlobStore.createWriteStream({
-            key: req.user.name,
-            params: {
-                ACL: 'public-read',
-                CacheControl: 'max-age=31536000'
-            }
-        }))
-        .on('error', console.error.bind(console))
-        .on('finish', async (file) => {
-            console.log('finish', file);
-            await um.updateAvatar(req.user.id, `${global.nconf.get('s3:endpoint')}/images-guac/profile-avatars/${file.key}`);
-        })
+		}
+		const { profilePicBlobStore } = req.s3;
+		console.log('profilePicBlobStore', profilePicBlobStore);
+		
+		const stream = Readable.from(req.file.buffer.toString());
+		stream.pipe(profilePicBlobStore.createWriteStream({
+			key: req.user.name,
+			params: {
+				ACL: 'public-read',
+				CacheControl: 'max-age=31536000'
+			}
+		}))
+		.on('error', console.error.bind(console))
+		.on('finish', async (file) => {
+			console.log('finish', file);
+			await um.updateAvatar(req.user.id, `${global.nconf.get('s3:endpoint')}/images-guac/profile-avatars/${file.key}`);
+		})
 		return send(res, 200, {
 			statusCode: 200,
 		});
