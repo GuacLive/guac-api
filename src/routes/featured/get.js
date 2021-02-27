e3ed27ee84d7d37b83306e255fb1d0abca1f1749
Thomas Lekanger <datagutt@lekanger.no>
2021-02-27 20:18:43 +0100

    Update featured

--- src/routes/featured/get.js
+++ src/routes/featured/get.js
@@ -1,7 +1,7 @@
-import { compose } from 'micro-hoofs';
+import {compose} from 'micro-hoofs';
 import cache from 'micro-cacheable';
 
-import { getFromViewerAPI } from '../../utils';
+import {getFromViewerAPI} from '../../utils';
 import streamModel from '../../models/stream';
 
 // Cache response for 10 seconds
@@ -11,15 +11,24 @@ module.exports = cache(10 * 1000, compose(
 		const stream = new streamModel;
 		const results = await stream.getFeatured();
 		const data = await Promise.all(results.map(async (item) => {
-				//await stream.increaseView(item.id);
-				item = await stream.getStream(item.name);
-				return {
+			//await stream.increaseView(item.id);
+			item = await stream.getStream(item.name);
+			return {
+				statusCode: 200,
+				data: {
 					id: item.id,
 					name: item.name,
+					type: item.stream_type,
 					title: item.title,
 					live: parseInt(item.live, 10),
+					liveAt: item.time,
+					followers: await stream.getStreamFollowCount(item.user_id),
 					viewers: item.live ? await getFromViewerAPI(item.name) : 0,
 					views: parseInt(item.views, 10),
+					private: item.private,
+					category_id: item.category_id,
+					category_name: item.category_name,
+					banner: item.banner,
 					urls: {
 						hls: `/live/${item.name}/abr.m3u8`,
 						flv: `/live/${item.name}.flv`
@@ -32,18 +41,19 @@ module.exports = cache(10 * 1000, compose(
 						//'360p': '_low',
 						//'240p': '_mobile'
 					},
+					// DEPRECATED - Servers-field (KEEP UNTIL FRONTEND UPDATED)
 					servers: global.nconf.get('server:streaming_servers'),
-					category_id: item.category_id,
-					category_name: item.category_name,
+					streamServer: item.streamServer,
 					user: {
 						id: item.user_id,
 						name: item.name,
 						type: item.type,
-						avatar: item.avatar || `//api.${global.nconf.get('server:domain')}/avatars/unknown.png`,
+						avatar: item.avatar || `${global.nconf.get('s3:cdn_endpoint')}/profile-avatars/offline-avatar.png`,
 						banned: item.banned
-					}
-				};
-			})
+					},
+				}
+			};
+		})
 			.sort((a, b) => {return a.viewers < b.viewers;})
 		);
 		return {
