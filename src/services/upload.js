fc5e8fce81c1bda7c453c7db4dbb1f0ab8bc455f
Thomas Lekanger <datagutt@lekanger.no>
2021-03-05 16:55:29 +0100

    Upload service: Fix for linode object storage multipart

--- src/services/upload.js
+++ src/services/upload.js
@@ -1,10 +1,17 @@
 import multer from 'multer';
 import store from 's3-blob-store';
-import {Endpoint, S3} from 'aws-sdk';
+import {Endpoint, S3, util} from 'aws-sdk';
 
 export default fn => async (req, res) => {
     const multipartMiddleware = multer();
 
+    // Fix for Linode object storage error
+    util.update(S3.prototype, {
+        addExpect100Continue: function addExpect100Continue(req) {
+            console.log('Depreciating this workaround, because introduced a bug');
+            console.log('Check: https://github.com/andrewrk/node-s3-client/issues/74');
+        }
+    });
     const s3Endpoints = new Endpoint(global.nconf.get('s3:endpoint'));
     const s3 = new S3({
         accessKeyId: global.nconf.get('s3:access_key'),
