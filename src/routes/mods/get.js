c987de29e41173d91ee9053073fea8faf8120814
datagutt <datagutt@lekanger.no>
2021-08-04 23:29:35 +0200

    Public endpoint for getting channel mods

---
+++ src/routes/mods/get.js
@@ -0,0 +1,33 @@
+import {send} from 'micro';
+import {compose} from 'micro-hoofs';
+
+import channelModel from '../../models/channel';
+
+import {USERNAME_REGEX} from '../../utils';
+
+module.exports = compose(
+)(
+	async (req, res) => {
+		if (!req.params.name || !USERNAME_REGEX.test(req.params.name)) {
+			return send(res, 400, {
+				statusCode: 400,
+				statusMessage: 'No stream name given'
+			});
+		}
+		const stream = new streamModel;
+		const streamResult = await stream.getStream(req.params.name);
+		if (streamResult && streamResult.user_id) {
+			const channel = new channelModel;
+			const result = await channel.getMods(streamResult.user_id);
+			send(res, 200, {
+				statusCode: 200,
+				...result
+			});
+		} else {
+			send(res, 404, {
+				statusCode: 404,
+				statusMessage: 'Stream not found'
+			});
+		}
+	}
+);
